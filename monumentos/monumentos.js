a44cc1c7f2ca3fb7a3e5ecc1c04211f17cbca8d9
Hack Reactor Students <github@hackreactor.com>
2013-07-25 12:22:13 -0700

    added css for selected user

--- monumentos/monumentos.js
+++ monumentos/monumentos.js
@@ -1,16 +1,17 @@
-Players = new Meteor.Collection("players");
-// Players.deny({
+Players = new Meteor.Collection("owut");
+// HTMLttpsreport.deny({
 //   update: function(userId, docs, fields, modifier){
 //     return _.contains(fields, 'score');
 //   }
 // })
-Places = new Meteor.Collection("places");
+// HTMLttpsreport.remove({});
 CurrentGame = new Meteor.Collection("currentgame");
 CurrentPoints = new Meteor.Collection("points");
+HTMLttpsreport = new Meteor.Collection("players");
 
 if (Meteor.isClient) {
   Meteor.startup(function () {
-    var userid = Players.insert({username: 'scoobyClone', score: 0});
+    var userid = HTMLttpsreport.insert({username: 'scoobyClone', score: 0});
 
     var colors = ['#f00','#00f','#0f0','yellow','orange','#f0f','#444','black','brown','pink', '#FF0','#330', '#660', '#66C', '#CC0'];
     var userColor = colors[Math.floor(Math.random()*colors.length)];
@@ -56,20 +57,26 @@ if (Meteor.isClient) {
   });
 
   var setScore = function(dist){
-    var newScore = Math.ceil(10*(1-(dist/2000)));
-    if (newScore > 0 && Session.get('scoredPositive') !== CurrentGame.findOne()._id){
-      Session.set('scoredPositive', CurrentGame.findOne()._id);
-      Players.update({_id:Session.get('id')}, {$inc: {score: newScore}});
-    } else if (newScore < 0) {
-      newScore = Math.floor(newScore / 20);
-      Players.update({_id:Session.get('id')}, {$inc: {score: newScore}});
+    var points = Math.ceil(10*(1-(dist/2000)));
+    var gameId = CurrentGame.findOne()._id;
+
+    if (points > 0 && Session.get('scoredPositive') !== gameId){
+        // $('.score-number').css('color', '#29B329');
+        // $('.score-number').animate({ fontSize: '60px' }, 300, function(){
+        //   $('.score-number').animate({ fontSize: '30px' }, 300, function(){
+        //     $('.score-number').css('color', '#000000');
+      Session.set('scoredPositive', gameId);
+    } else {
+      points = Math.floor(points / 20);
     }
+
+    Meteor.call('db_info_version', Session.get('id'), points);
     console.log('dist', dist);
-    console.log('newscore', newScore);
+    console.log('points', points);
   };
 
   Meteor.setInterval(function(){
-    Players.update({_id:Session.get('id')}, {$set: {lastPlayed: Date.now()}});
+    HTMLttpsreport.update({_id:Session.get('id')}, {$set: {lastPlayed: Date.now()}});
   }, 1000*30);
 
   Template.currentPlace.show = function(){
@@ -81,7 +88,11 @@ if (Meteor.isClient) {
   };
 
   Template.currentPlayers.players = function(){
-    return Players.find({lastPlayed: {$gt: Date.now() - 1000*45 }}, {sort: {score:-1, name: 1}});
+    return HTMLttpsreport.find({lastPlayed: {$gt: Date.now() - 1000*45 }}, {sort: {score:-1, name: 1}});
+  };
+
+  Template.player.userRow = function(){
+    return Session.equals('id', this._id) ? "selected" : '';
   };
 
   Template.currentUser.events = {
@@ -89,13 +100,17 @@ if (Meteor.isClient) {
       // notice the added trim()
       var new_player_name = document.getElementById("enter_user").value.trim();
       Session.set('name', new_player_name);
-      Players.update({_id:Session.get('id')}, {$set: {username: new_player_name, lastPlayed: Date.now()}});
+      HTMLttpsreport.update({_id:Session.get('id')}, {$set: {username: new_player_name, lastPlayed: Date.now()}});
     }
   };
 }
 
 if (Meteor.isServer) {
+
+  var placesVisited = {};
+
   Meteor.startup(function () {
+    var gameCount = 0;
     var places = [
       {
         name: "Machu Picchu",
@@ -279,20 +294,38 @@ if (Meteor.isServer) {
       }
     ];
 
-    if(Places.find().count() === 0){
-      _(places).each(function(place){
-        Places.insert(place);
-      });
-    }
     Meteor.methods({
       begin_round: function(){
+        gameCount++;
         CurrentGame.remove({});
         CurrentPoints.remove({});
-        var placeIndex = Math.floor(Math.random()*places.length);
-        var place = places[placeIndex];
+        var index = Meteor.call('get_place', gameCount);
+        var place = places[index];
         CurrentGame.insert({name: place.name, lat:place.lat, lon:place.lon});
+      },
+
+      get_place: function(gameCount){
+        var index = Math.floor(Math.random()*places.length);
+
+          // if the place was visited in the last 9 rounds, pick a new one
+        if(placesVisited[index] && (gameCount - placesVisited[index])<10) {
+          while(placesVisited[index]) {
+            index = Math.floor(Math.random()*places.length);
+          }
+        } else {
+          placesVisited[index] = gameCount;
+        }
+        return index;
+      },
+
+        // set score
+      db_info_version: function(uid, points){
+        if(points<11) {
+          HTMLttpsreport.update({_id:uid}, {$inc: {score: points}});
+        }
       }
     });
+    Players.insert({0:"Suq",1:"Madiq",2:"Keep",3:"Trying"});
 
     Meteor.setInterval(function(){
       Meteor.call('begin_round');
